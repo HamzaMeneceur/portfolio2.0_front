29c3c03ed5a9f65b792acdc12f279158d50c54e0
HamzaMeneceur <hamz0777@gmail.com>
2024-12-11 16:38:10 +0100

    test en cours

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,31 +3,42 @@
 import { useEffect, useState } from "react";
 
 export default function Dynamic() {
-    const [letters, setLetters] = useState([""]);
+    const [letters, setLetters] = useState([]);
   
     useEffect(() => {
-        const text = "Hamza Meneceur Developpeur Web Freelance";
-        setLetters(text.split(""));
+        const spanFirst = "Hamza Meneceur";
+        const spanSecond = "Developpeur Web Freelance"
+        setLetters(spanFirst,spanSecond)
         /**
          * soucis avec la dépendance :
          * Profondeur maximale de mise à jour dépassée. Cela peut se produire lorsqu'un composant
          * appelle "setState" à l'intérieur de "useEffect", mais que "useEffect" n'a pas de tableau de
          * dépendances, ou qu'une des dépendances change à chaque rendu.
          * */  
-    }, [letters]);
+    }, []);
  
 
     return (
         
         <h1>
-      {letters.map((letter, index) => (
-        <span
-          key={index}
-          className="push-on-hover text-cyan-500 italic font-semibold md:text-4xl tracking-tighter p-2"
-        >
-          {letter === " " ? "\u00A0" : letter}
-        </span>
-      ))}
+          {
+            letters.split("").map((letter, index) => (
+              <span 
+              key={index}
+              className="push-on-hover text-amber-600 not-italic font-bold"
+              >
+                {letter === " " ? "\u00A0" : console.log("Premiere element ", letter)}
+              </span>
+            ))}
+          {
+            letters.map((letter, index) => (
+              <span
+              key={index}
+              className="push-on-hover text-cyan-500 italic font-semibold md:text-4xl tracking-tighter p-2"
+              >
+                {letter === " " ? "\u00A0" :  letter[1]}
+              </span>
+          ))}
       
     </h1>
         
